e93f54103ec34e9bd24f440cb502107579c2c46c
Peter Borah <peterborah@gmail.com>
2015-09-24 02:44:15 -0500

    Get test passing.

--- test/proxy.js
+++ test/proxy.js
@@ -6,7 +6,7 @@ contract('Proxy', function(accounts) {
     Proxy.new(controller.address).
       then(function(proxy) {
         controller.set_parameters(target.address, 1000).
-        then(function() { web3.eth.sendTransaction({from: accounts[0], to: proxy.address, amount:1000}, function() {
+        then(function() { web3.eth.sendTransaction({from: accounts[0], to: proxy.address, value:1000}, function() {
         web3.eth.sendTransaction({from: accounts[0], to: proxy.address}, function() {
           target.times_called.call().
           then(function(result) { assert.equal(result, 1) }).
